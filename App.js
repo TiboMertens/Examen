5a746db03cf3c134ae3acac5596ff10766d95cd8
TiboMertens <tibomertens25@gmail.com>
2022-12-30 16:08:11 +0100

    added app.js

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AllCarsScreen from './screens/AllCarsScreen';
+import Details from './screens/details';
+
+const Stack = createNativeStackNavigator();
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Cars" component={AllCarsScreen} />
+        <Stack.Screen name="Details" component={Details} />  
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
