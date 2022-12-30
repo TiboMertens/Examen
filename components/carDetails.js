c8143877775adf3a003324830f401e67fb4e2a40
TiboMertens <tibomertens25@gmail.com>
2022-12-30 21:59:05 +0100

    added styling

--- components/carDetails.js
+++ components/carDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {
-  Stylesheet,
+  StyleSheet,
   Text,
   View,
   Image,
@@ -11,15 +11,43 @@ import {
 
 const Detail = (props) => {
   return (
-    <View>
-      <Text>{props.title}</Text>
-      <Text>{props.description}</Text>
+    <View style={styles.container}>
       <Image
         style={{ width: 420, height: 200 }}
         source={{ uri: `${props.image}` }}
       />
+      <View style={styles.insideContainer}>
+        <Text style={styles.cartitle}>{props.title}</Text>
+        <Text style={styles.carDesc}>{props.description}</Text>
+        <Text style={styles.author}>Author: {props.author}</Text>
+      </View>
     </View>
   );
 };
 
 export default Detail;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#353935",
+    paddingBottom: 480,
+  },
+  cartitle: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 24,
+    marginTop: 10,
+  },
+  insideContainer: {
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  carDesc: {
+    color: "#ffffff",
+    fontWeight: "300",
+    marginTop: 2,
+  },
+  author: {
+    color: "#ffffff"
+  }
+});
