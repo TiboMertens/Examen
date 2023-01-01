6fdbd1ca944e46a9ad5e9078fdddeeaabd9048d7
TiboMertens <tibomertens25@gmail.com>
2023-01-01 20:08:45 +0100

    added searchbar

---
+++ components/searchFilter.js
@@ -0,0 +1,169 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Image,
+  Pressable,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+
+const searchFilter = ({ navigation, data, input, setInput }) => {
+  const [counter, setCounter] = useState(0);
+  console.log(input)
+  const increase = () => {
+    setCounter((currentCounter) => currentCounter + 1);
+  };
+
+  return (
+    <View>
+      <View style={styles.shoppingCart}>
+        <Image
+          style={styles.icon}
+          source={require("../assets/shoppingCart.png")}
+        />
+        <Text style={styles.shoppingCartAmount}>{counter}</Text>
+      </View>
+      <FlatList
+        style={styles.list}
+        data={data}
+        renderItem={({ item }) => {
+          if (input === "") {
+            return (
+              <View style={styles.Container}>
+                <View style={styles.carsContainer}>
+                  {item.yoast_head_json?.og_image !== undefined && (
+                    <Image
+                      style={styles.image}
+                      source={{
+                        uri: `${item.yoast_head_json.og_image[0].url}`,
+                      }}
+                    />
+                  )}
+                  <Text style={styles.carTitle}>{item.title.rendered}</Text>
+
+                  <Pressable
+                    onPress={() =>
+                      navigation.navigate("Details", {
+                        itemTitle: item.title.rendered,
+                        itemDesc: item.rttpg_excerpt,
+                        itemAuthor: item.yoast_head_json.author,
+                        itemImage: item.yoast_head_json.og_image[0].url,
+                      })
+                    }
+                  >
+                    <View style={styles.buttonsContainer}>
+                      <Text style={styles.bekijkBtn}>bekijk product</Text>
+                      <Pressable onPress={increase}>
+                        <Text style={styles.cartBtn}>Add to cart</Text>
+                      </Pressable>
+                    </View>
+                  </Pressable>
+                </View>
+              </View>
+            );
+          }
+
+          if (item.title.rendered.toLowerCase().includes(input.toLowerCase())) {
+            return (
+                <View style={styles.Container}>
+                <View style={styles.carsContainer}>
+                  {item.yoast_head_json?.og_image !== undefined && (
+                    <Image
+                      style={styles.image}
+                      source={{
+                        uri: `${item.yoast_head_json.og_image[0].url}`,
+                      }}
+                    />
+                  )}
+                  <Text style={styles.carTitle}>{item.title.rendered}</Text>
+
+                  <Pressable
+                    onPress={() =>
+                      navigation.navigate("Details", {
+                        itemTitle: item.title.rendered,
+                        itemDesc: item.rttpg_excerpt,
+                        itemAuthor: item.yoast_head_json.author,
+                        itemImage: item.yoast_head_json.og_image[0].url,
+                      })
+                    }
+                  >
+                    <View style={styles.buttonsContainer}>
+                      <Text style={styles.bekijkBtn}>bekijk product</Text>
+                      <Pressable onPress={increase}>
+                        <Text style={styles.cartBtn}>Add to cart</Text>
+                      </Pressable>
+                    </View>
+                  </Pressable>
+                </View>
+              </View>
+            );
+          }
+        }}
+      ></FlatList>
+    </View>
+  );
+};
+
+export default searchFilter;
+
+const styles = StyleSheet.create({
+  shoppingCartAmount: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    marginLeft: 27,
+    position: "relative",
+    bottom: 7,
+  },
+  icon: {
+    width: 25,
+    height: 25,
+  },
+  shoppingCart: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 20,
+  },
+  image: {
+    width: 300,
+    height: 200,
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 3,
+  },
+  list: {
+    marginTop: 5,
+  },
+  carsContainer: {
+    width: 350,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 50,
+    paddingTop: 25,
+    paddingBottom: 25,
+    backgroundColor: "#545454",
+    borderRadius: 10,
+  },
+  carTitle: {
+    color: "#ffffff",
+    fontSize: 20,
+    marginLeft: 25,
+    fontWeight: "bold",
+  },
+  bekijkBtn: {
+    color: "#61CE70",
+    fontWeight: "700",
+    marginTop: 5,
+    marginLeft: 25,
+    marginRight: 138,
+  },
+  buttonsContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  cartBtn: {
+    color: "#ffffff",
+    marginTop: 5,
+  },
+});
--- screens/AllCarsScreen.js
+++ screens/AllCarsScreen.js
@@ -9,14 +9,11 @@ import {
   FlatList,
 } from "react-native";
 
+import SearchFilter from "../components/searchFilter";
 
 const Cars = ({ navigation }) => {
   const [counter, setCounter] = useState(0);
   const [Cars, setCars] = useState([]);
-
-  const increase = () => {
-    setCounter((currentCounter) => currentCounter + 1);
-}
 
   const getCars = async () => {
     try {
@@ -35,48 +32,18 @@ const Cars = ({ navigation }) => {
     getCars();
   }, []);
 
+  const [input, setInput] = useState("");
   return (
     <View style={styles.container}>
-      <View style={styles.shoppingCart}>
-        <Image
-          style={styles.icon}
-          source={require("../assets/shoppingCart.png")}
+      <View style={styles.searchContainer}>
+        <TextInput
+          value={input}
+          onChangeText={(text) => setInput(text)}
+          style={{ fontSize: 16, marginLeft: 10 }}
+          placeholder="Search"
         />
-        <Text style={styles.shoppingCartAmount}>{counter}</Text>
       </View>
-      <FlatList
-        style={styles.list}
-        data={Cars}
-        renderItem={({ item }) => (
-          <View style={styles.carsContainer}>
-            {item.yoast_head_json?.og_image !== undefined && (
-              <Image
-                style={styles.image}
-                source={{ uri: `${item.yoast_head_json.og_image[0].url}` }}
-              />
-            )}
-            <Text style={styles.carTitle}>{item.title.rendered}</Text>
-
-            <Pressable
-              onPress={() =>
-                navigation.navigate("Details", {
-                  itemTitle: item.title.rendered,
-                  itemDesc: item.rttpg_excerpt,
-                  itemAuthor: item.yoast_head_json.author,
-                  itemImage: item.yoast_head_json.og_image[0].url,
-                })
-              }
-            >
-              <View style={styles.buttonsContainer}>
-                <Text style={styles.bekijkBtn}>bekijk product</Text>
-                <Pressable onPress={(increase)}>
-                <Text style={styles.cartBtn}>Add to cart</Text>
-                </Pressable>
-              </View>
-            </Pressable>
-          </View>
-        )}
-      ></FlatList>
+      <SearchFilter data={Cars} input={input} setInput={setInput} navigation={navigation} />
     </View>
   );
 };
@@ -84,66 +51,16 @@ const Cars = ({ navigation }) => {
 export default Cars;
 
 const styles = StyleSheet.create({
-  image: {
-    width: 300,
-    height: 200,
-    marginLeft: "auto",
-    marginRight: "auto",
-    borderRadius: 3,
-  },
-  shoppingCartAmount: {
-    marginTop: 37,
-    marginLeft: 2,
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#ffffff",
-  },
-  shoppingCart: {
-    display: "flex",
-    flexDirection: "row",
-    position: "absolute",
-    right: 32,
-  },
-  icon: {
-    width: 25,
-    height: 25,
-    marginTop: 20,
-  },
-  list: {
-    marginTop: 75,
-  },
-  carsContainer: {
-    width: 350,
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginBottom: 50,
-    paddingTop: 25,
-    paddingBottom: 25,
-    backgroundColor: "#545454",
-    borderRadius: 10,
-  },
   container: {
-    backgroundColor: "#353935",
+    backgroundColor: "#353935"
   },
-  carTitle: {
-    color: "#ffffff",
-    fontSize: 20,
-    marginLeft: 25,
-    fontWeight: "bold",
+  searchContainer: {
+    backgroundColor: "#Eaf0f3",
+    width: "83.5%",
+    marginLeft: 30,
+    marginTop: 20,
+    borderRadius: 7,
+    paddingTop: 2,
+    paddingBottom: 2,
   },
-  bekijkBtn: {
-    color: "#61CE70",
-    fontWeight: "700",
-    marginTop: 5,
-    marginLeft: 25,
-    marginRight: 138,
-  },
-  buttonsContainer: {
-    display: "flex",
-    flexDirection: "row",
-  },
-  cartBtn: {
-    color: "#ffffff",
-    marginTop: 5,
-  }
 });
