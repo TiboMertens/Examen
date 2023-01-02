3d690162ef671f484befa7f16b39863a69ac1e12
TiboMertens <tibomertens25@gmail.com>
2023-01-02 20:34:13 +0100

    added basic syntax

---
+++ components/shoppingCart.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native'
+import React, { useState, useEffect } from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <View>
+      <Text>shoppingCart</Text>
+    </View>
+  )
+}
+
+export default ShoppingCart
+
+const styles = StyleSheet.create({})
