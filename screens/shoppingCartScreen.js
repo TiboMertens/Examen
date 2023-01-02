7245b9b3dfb5e6daeb53f630c75075585d209121
TiboMertens <tibomertens25@gmail.com>
2023-01-02 20:34:51 +0100

    added basic syntax & imported component

---
+++ screens/shoppingCartScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import ShoppingCart from '../components/shoppingCart'
+
+const shoppingCartScreen = () => {
+  return (
+    <ShoppingCart />
+  )
+}
+
+export default shoppingCartScreen
+
+const styles = StyleSheet.create({})
