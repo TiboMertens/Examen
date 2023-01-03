7adaf8f20cb4dd6cc49782a45e8263dc77b999d5
TiboMertens <tibomertens25@gmail.com>
2023-01-03 16:31:56 +0100

    made login screen

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import AllCarsScreen from './screens/AllCarsScreen';
-import Details from './screens/details';
-import ReviewScreen from './screens/ReviewScreen';
+import AllCarsScreen from "./screens/AllCarsScreen";
+import Details from "./screens/details";
+import ReviewScreen from "./screens/ReviewScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Cars" component={AllCarsScreen} />
-        <Stack.Screen name="Details" component={Details} />  
-        <Stack.Screen name ="Reviews" component={ReviewScreen} />
+        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Reviews" component={ReviewScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
